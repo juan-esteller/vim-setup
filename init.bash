e92999cb11be8df019ca87fdd35e656e38c73a93
Juan Esteller <esteller@ice2.fas.harvard.edu>
2018-01-09 18:56:07 -0500

    now have initialization script working

--- init.bash
+++ init.bash
@@ -1,4 +1,4 @@
-mv .vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 mkdir ~/.vim
 cd ~/.vim
 mkdir autoload 
@@ -6,8 +6,11 @@ git clone git@github.com:tpope/vim-pathogen.git
 mv vim-pathogen/autoload/pathogen.vim autoload/pathogen.vim
 rm -rf vim-pathogen 
 mkdir bundle 
-mkdir plugin
+mkdir plugin 
 mkdir ftplugin 
 cd bundle 
 git clone git@github.com:ctrlpvim/ctrlp.vim.git
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone git@github.com:vim-airline/vim-airline.git
+git clone git@github.com:easymotion/vim-easymotion.git
+git clone git@github.com:tpope/vim-surround.git
